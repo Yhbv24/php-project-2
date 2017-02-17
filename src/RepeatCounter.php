7c33bd26447d22f4e9f132efef745c63e0976ec8
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-02-16 18:40:56 -0800

    Add classes and first test method

---
+++ src/RepeatCounter.php
@@ -0,0 +1,32 @@
+<?php
+    class RepeatCounter
+    {
+        private $input_word;
+        private $input_phrase;
+
+        function setInputWord($new_input_word)
+        {
+            $this->input_word = $new_input_word;
+        }
+
+        function getInputWord()
+        {
+            return $this->input_word;
+        }
+
+        function setInputPhrase($new_input_phrase)
+        {
+            $this->input_phrase = $new_input_phrase;
+        }
+
+        function getInputPhrase()
+        {
+            return $this->input_phrase;
+        }
+
+        function CountRepeats()
+        {
+            
+        }
+    }
+?>
---
+++ tests/RepeatCounterTest.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest expands PHPUnit_Framework_TestCase
+    {
+        function test_getCount_letter()
+        {
+            // Arrange
+            $test_getCountLetter = new RepeatCounter;
+            $input = "b";
+
+            // Act
+            $result = $test_getCountLetter->CountRepeats($input);
+
+            // Assert
+            $this->assertEquals(4, $result);
+        }
+    }
+?>
